36f57a1b12e32cac87bb640864eef46648ad9544
tyler <tylersharara@gmail.com>
2020-07-14 02:38:37 -0400

    Tyler Sharara
    - added moment
    - added fs
    - gave the user an option to start a new game

--- Homework/node/index.js
+++ Homework/node/index.js
@@ -7,17 +7,23 @@
 */
 
 const prompt = require('prompt-sync')();
+const moment = require('moment');
+const fs = require('fs');
 
 function numberGame() {
-    var num = Math.floor(Math.random() * 101);
-    var flag = false;
-    var guess = 0;
+    let num = Math.floor(Math.random() * 101);
+    time = moment().format('MMMM Do YYYY, h:mm:ss a');
+    let flag = false;
+    let guess = 0;
 
     do {
         const num2 = prompt('Guess a number between 1-100:');
         if (num == num2) {
             guess++;
             flag = true;
+            fs.appendFile('gamer.txt', `You beat the guessing game in ${guess} guesses on ${time + '\n'}`, function (err) {
+                if (err) throw err;
+            });
             console.log(`Good Job! It took you: ${guess} guesses!`, '\n');
         } else if (num > num2) {
             console.log(`Too low!`, '\n');
@@ -29,8 +35,14 @@ function numberGame() {
     } while (flag == false);
 }
 
-while (true == true){
+let flag = false;
+
+do {
     numberGame();
-}
+    const play = prompt('Wanna play again? (Y/N)');
+    if(play == 'N' || play == 'n'){
+        flag = true;
+    }
+} while (flag == false);
 
 
